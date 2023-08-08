1b721ddd8cff99b05b4755fcdf1d9b6aac996271
Elmar Ismayilov <bamazy@gmail.com>
2023-08-08 18:46:15 +0800

    refactoring

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,40 +1,50 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
           enhanceComponent: (Component) => Component,
-				});
-			const initialProps = await Document.getInitialProps(ctx);
+        });
+      const initialProps = await Document.getInitialProps(ctx);
 
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				)
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
-	render() {
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
     return (
       <Html lang="en">
         <Head />
-        <body> 
+        <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }

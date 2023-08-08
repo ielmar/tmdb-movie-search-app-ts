f991d8d391afeb7e83a35fe76e4aaf21b772aa43
Elmar Ismayilov <bamazy@gmail.com>
2023-08-09 06:57:00 +0800

    conditional showing of components added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,33 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Layout from '@/components/Layout';
 import Search from '@/components/Search';
+import Results from '@/components/Results';
 
 const Heading = styled.h2`
   font-weight: 600;
 `;
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
   return (
     <Layout>
-      <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56">
+      <div className="mx-auto max-w-7xl py-22 sm:py-48 lg:py-42">
         <div className="text-center">
-          <Search />
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Discover the cinematic universe at #1 Movie Search Engine. Dive into
-            a vast collection of films, find new favorites, and connect with
-            fellow movie buffs. Stay updated, personalize your watchlist, and
-            fuel your passion for film. Your gateway to cinematic magic has
-            arrived.
-          </p>
+          <Search setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+          {!searchTerm && (
+            <p className="mt-10 text-lg leading-8 text-gray-600">
+              Discover the cinematic universe at #1 Movie Search Engine. Dive
+              into a vast collection of films, find new favorites, and connect
+              with fellow movie buffs. Stay updated, personalize your watchlist,
+              and fuel your passion for film. Your gateway to cinematic magic
+              has arrived.
+            </p>
+          )}
         </div>
+        <Results searchTerm={searchTerm} />
       </div>
     </Layout>
   );
